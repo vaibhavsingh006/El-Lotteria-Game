e1d1aa2cc0ac8788fa2883011ed1b2825dd31036
Vaibhav Singh <122105010+vaibhavsingh006@users.noreply.github.com>
2024-11-03 02:58:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,18 @@ const cors = require('cors');
 const app = express();
 const gameRoutes = require('./routes/gameRoutes')
 
-app.use(cors());
+const allowedOrigins = [
+    'https://el-lotteria-game-2pwu.onrender.com', // Add your frontend URL here
+];
+
+const corsOptions = {
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow credentials if needed
+    optionsSuccessStatus: 200 // For legacy browser support
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/lotteryGame')
