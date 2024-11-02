6da46b73da9494f96b90aefa6577c61692655531
Vaibhav Singh <122105010+vaibhavsingh006@users.noreply.github.com>
2024-11-03 03:59:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,6 +5,14 @@ const cors = require('cors');
 const app = express();
 const gameRoutes = require('./routes/gameRoutes')
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://el-lotteria-game-nsub.onrender.com");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+
 // Serve static files from the React app's build directory
 app.use(express.static(path.join(__dirname, 'build')));
 
