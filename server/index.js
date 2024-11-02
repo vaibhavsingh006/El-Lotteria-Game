6c297b4894c619a2bbb4febec2b3c181bef0a527
Vaibhav Singh <122105010+vaibhavsingh006@users.noreply.github.com>
2024-11-03 03:24:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const cors = require('cors');
 const app = express();
 const gameRoutes = require('./routes/gameRoutes')
+
+// Serve static files from the React app's build directory
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Catch-all route to serve the React app for any other requests
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+});
 
 const allowedOrigins = [
     'https://el-lotteria-game-nsub.onrender.com/', // Add your frontend URL here
