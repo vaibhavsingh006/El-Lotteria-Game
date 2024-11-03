3f95415ddd40b45f4b9b47b392a9fbd0d2542e1b
Vaibhav Singh <122105010+vaibhavsingh006@users.noreply.github.com>
2024-11-03 14:14:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path');
 const cors = require('cors');
 const app = express();
 const gameRoutes = require('./routes/gameRoutes')
@@ -8,28 +8,7 @@ const gameRoutes = require('./routes/gameRoutes')
 app.use(cors());
 app.use(express.json());
 
-app.options('/api/start-game', cors()); 
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://el-lotteria-game-nsub.onrender.com");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
-app.use(cors({
-  origin: '*', // This allows all origins
-}));
-
-// Serve static files from the React app's build directory
-app.use(express.static(path.join(__dirname, 'build')));
-
-// Catch-all route to serve the React app for any other requests
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/build/index.html'));
-});
-
-
+// mongoose.connect(process.env.MONGODB_URI)
 mongoose.connect('mongodb://localhost:27017/lotteryGame')
     .then(() => console.log('MongoDB connect'))
     .catch(err => console.log('MongoDB not connected', err));
@@ -39,6 +18,8 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api', gameRoutes)
+
+// const PORT = process.env.PORT || 5000;
 
 app.listen(5000, () => {
     console.log('Server is running prot 5000')
